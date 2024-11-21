a09d2bb56dc3f75481b929d291ef9277d44e808d
Ebtekar.Ali <Ebtekar.Ali@C19-05>
2024-11-21 20:36:13 +0100

    Final Version of Falling Apple code

--- AppleTimer/Form1.Designer.cs
+++ AppleTimer/Form1.Designer.cs
@@ -38,6 +38,14 @@ namespace AppleTimer
             this.fej = new System.Windows.Forms.PictureBox();
             this.test = new System.Windows.Forms.PictureBox();
             this.kez = new System.Windows.Forms.PictureBox();
+            this.AppleCounter = new System.Windows.Forms.Label();
+            this.storageLabel = new System.Windows.Forms.Label();
+            this.buybiggerstorgae = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buyfasterhit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Flor)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.kosar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -99,7 +107,7 @@ namespace AppleTimer
             // alma
             // 
             this.alma.BackColor = System.Drawing.Color.Red;
-            this.alma.Location = new System.Drawing.Point(170, 139);
+            this.alma.Location = new System.Drawing.Point(135, 83);
             this.alma.Name = "alma";
             this.alma.Size = new System.Drawing.Size(23, 25);
             this.alma.TabIndex = 5;
@@ -135,18 +143,107 @@ namespace AppleTimer
             this.kez.TabIndex = 8;
             this.kez.TabStop = false;
             // 
+            // AppleCounter
+            // 
+            this.AppleCounter.AutoSize = true;
+            this.AppleCounter.BackColor = System.Drawing.Color.Yellow;
+            this.AppleCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.AppleCounter.Location = new System.Drawing.Point(262, 37);
+            this.AppleCounter.Name = "AppleCounter";
+            this.AppleCounter.Size = new System.Drawing.Size(148, 16);
+            this.AppleCounter.TabIndex = 9;
+            this.AppleCounter.Text = "Apple Count Number: 0 ";
+            // 
+            // storageLabel
+            // 
+            this.storageLabel.AutoSize = true;
+            this.storageLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.storageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.storageLabel.Location = new System.Drawing.Point(477, 37);
+            this.storageLabel.Name = "storageLabel";
+            this.storageLabel.Size = new System.Drawing.Size(129, 16);
+            this.storageLabel.TabIndex = 10;
+            this.storageLabel.Text = "Basket Capacity : 20";
+            // 
+            // buybiggerstorgae
+            // 
+            this.buybiggerstorgae.BackColor = System.Drawing.Color.Sienna;
+            this.buybiggerstorgae.ForeColor = System.Drawing.SystemColors.Control;
+            this.buybiggerstorgae.Location = new System.Drawing.Point(648, 93);
+            this.buybiggerstorgae.Name = "buybiggerstorgae";
+            this.buybiggerstorgae.Size = new System.Drawing.Size(84, 34);
+            this.buybiggerstorgae.TabIndex = 11;
+            this.buybiggerstorgae.Text = "10 Apple";
+            this.buybiggerstorgae.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(542, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Bigger Basket";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(542, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Extra 5 Apple";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(542, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "-1 Apple";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(536, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Faster Pickup";
+            // 
+            // buyfasterhit
+            // 
+            this.buyfasterhit.BackColor = System.Drawing.SystemColors.GrayText;
+            this.buyfasterhit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buyfasterhit.Location = new System.Drawing.Point(648, 163);
+            this.buyfasterhit.Name = "buyfasterhit";
+            this.buyfasterhit.Size = new System.Drawing.Size(84, 33);
+            this.buyfasterhit.TabIndex = 14;
+            this.buyfasterhit.Text = "20 Apple";
+            this.buyfasterhit.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kosar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buyfasterhit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buybiggerstorgae);
+            this.Controls.Add(this.storageLabel);
+            this.Controls.Add(this.AppleCounter);
             this.Controls.Add(this.kez);
             this.Controls.Add(this.test);
             this.Controls.Add(this.fej);
             this.Controls.Add(this.alma);
             this.Controls.Add(this.lomb);
             this.Controls.Add(this.torzs);
-            this.Controls.Add(this.kosar);
             this.Controls.Add(this.Flor);
             this.Controls.Add(this.pictureBox3);
             this.Name = "Form1";
@@ -161,6 +258,7 @@ namespace AppleTimer
             ((System.ComponentModel.ISupportInitialize)(this.test)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.kez)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -175,6 +273,14 @@ namespace AppleTimer
         private System.Windows.Forms.PictureBox fej;
         private System.Windows.Forms.PictureBox test;
         private System.Windows.Forms.PictureBox kez;
+        private System.Windows.Forms.Label AppleCounter;
+        private System.Windows.Forms.Label storageLabel;
+        private System.Windows.Forms.Button buybiggerstorgae;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buyfasterhit;
     }
 }
 
--- AppleTimer/Form1.cs
+++ AppleTimer/Form1.cs
@@ -12,9 +12,198 @@ namespace AppleTimer
 {
     public partial class Form1 : Form
     {
+        Timer WalkTimer = new Timer();
+        Timer hitTimer = new Timer();
+        Timer gravity = new Timer();
+
+        int hitCount = 0;
+        int requiredHits = 10;
+        int hitDirection = 1; //1:barla;0;nyugalmai allapot; -1: Jobbra
+        int hitFrames = 0;
+        int hitMaxFrames = 3;
+        int apples = 0;
+        int storageCapacity = 20;
+
+        bool holdingAnApple = false;
+        int priceofBiggerStorage = 10;
+        int priceofFasterHit = 30;
+       
+
         public Form1()
         {
             InitializeComponent();
+            Start();
         }
+
+        void Start()
+        {
+            StartTimer();
+            AddEvent();
+        }
+        void AddEvent()
+        {
+            buybiggerstorgae.Click += BuyBiggerStorage;
+            buyfasterhit.Click += BuyFasterHit;
+        }
+        void StartTimer()
+        {
+            WalkTimer.Interval = 16;
+            hitTimer.Interval = 16;
+               gravity.Interval = 16;
+
+            WalkTimer.Start();
+            WalkTimer.Tick += WalkEvent;
+            hitTimer.Tick += HitEvent;
+            gravity.Tick += GravityEvent;
+
+
+        }
+
+        void WalkEvent(object s , EventArgs e)
+        {
+            if (kez.Left > torzs.Right && !holdingAnApple)
+            {
+
+            fej.Left -= 3;
+            test.Left -= 3;
+            kez.Left -= 3;
+            }
+            else if(!holdingAnApple)
+            {
+                WalkTimer.Stop();
+                hitDirection = 1;
+                hitTimer.Start();
+            }
+            else if (holdingAnApple && test.Right <kosar.Left)
+            {
+                fej.Left +=3;
+                test.Left+= 3;
+                kez.Left +=3;
+                alma.Left += 3;
+            }
+            else if(holdingAnApple && test.Right >= kosar.Left)
+            {
+                WalkTimer.Stop();
+                gravity.Start();
+            }
+
+        }
+
+        void HitEvent(object s, EventArgs e)
+        {
+            if (hitDirection == 1)
+            {
+                kez.Left -= 6;
+                hitFrames++;
+                if (hitFrames == hitMaxFrames)
+                {
+                    hitDirection = -1;
+                    hitFrames = 0;
+                }
+            }
+            else if (hitDirection == -1)
+            {
+                kez.Left += 6;
+                hitFrames++;
+                if (hitFrames == hitMaxFrames)
+                {
+                    hitFrames = 0;
+                    hitCount++;
+                    this.Text = hitCount.ToString();
+                    if (hitCount == requiredHits)
+                    {
+                        hitDirection = 0;
+                        hitTimer.Stop();
+                        alma.Left = kez.Left;
+                        alma.Top = lomb.Bottom - alma.Height;
+                        alma.Show();
+
+                        gravity.Start();
+                    }
+                    else
+                    {
+                        hitDirection = 1;
+                    }
+                }
+            }
+        }
+
+        void GravityEvent(object s, EventArgs e)
+        {
+            if(alma.Bottom < kez.Top && !holdingAnApple)
+            {
+
+            alma.Top += 3;
+            }
+            else if(!holdingAnApple)
+            {
+                holdingAnApple = true;
+                gravity.Stop();
+                kez.Left = test.Left + test.Width / 2;
+                alma.Left = kez.Right - alma.Width;
+                WalkTimer.Start();
+            }
+            else if (holdingAnApple)
+            {
+                alma.Top += 3;
+            }
+            if(alma.Top > kosar.Top)
+            {
+                gravity.Stop();
+                holdingAnApple = false;
+                kez.Left = test.Left + test.Width / 2 - kez.Width;
+
+                hitCount = 0;
+
+                if (apples < storageCapacity)
+                {
+
+                apples++;
+                }
+                updateAppleLable();
+
+                WalkTimer.Start();
+            }
+            
+
+        }
+        void BuyBiggerStorage(object s, EventArgs e)
+        {
+            if(apples > priceofBiggerStorage)
+            {
+                apples -= priceofBiggerStorage;
+                storageCapacity += 5;
+                priceofBiggerStorage += 2;
+                buybiggerstorgae.Text = $"{priceofBiggerStorage} apple";
+                updateAppleLable();
+                updateStorageLable();
+            }
+        }
+
+        void BuyFasterHit(object s, EventArgs e)
+        {
+            if(apples >= priceofFasterHit && requiredHits >3)
+            {
+                apples -= priceofFasterHit;
+                requiredHits--;
+                priceofFasterHit += 30;
+                buyfasterhit.Text = $"{priceofFasterHit} apple";
+
+                updateAppleLable();
+
+            }
+        }
+
+        void updateAppleLable()
+        {
+            AppleCounter.Text = $"Apple Count Number: {apples}";
+        }
+
+        void updateStorageLable()
+        {
+            storageLabel.Text = $"Apple Count Number: {storageCapacity} Apple";
+        }
+
+       
     }
 }
