fd947d5663785eba2658df76431cf1d25d2a4e5d
Ebtekar.Ali <Ebtekar.Ali@C19-05>
2024-11-21 17:27:37 +0100

    Add project files.

---
+++ AppleTimer/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace AppleTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Flor = new System.Windows.Forms.PictureBox();
+            this.kosar = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.torzs = new System.Windows.Forms.PictureBox();
+            this.lomb = new System.Windows.Forms.PictureBox();
+            this.alma = new System.Windows.Forms.PictureBox();
+            this.fej = new System.Windows.Forms.PictureBox();
+            this.test = new System.Windows.Forms.PictureBox();
+            this.kez = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Flor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.torzs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fej)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kez)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Flor
+            // 
+            this.Flor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Flor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Flor.Location = new System.Drawing.Point(-5, 408);
+            this.Flor.Name = "Flor";
+            this.Flor.Size = new System.Drawing.Size(810, 42);
+            this.Flor.TabIndex = 0;
+            this.Flor.TabStop = false;
+            // 
+            // kosar
+            // 
+            this.kosar.BackColor = System.Drawing.Color.Red;
+            this.kosar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.kosar.Location = new System.Drawing.Point(663, 367);
+            this.kosar.Name = "kosar";
+            this.kosar.Size = new System.Drawing.Size(100, 42);
+            this.kosar.TabIndex = 1;
+            this.kosar.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(703, 336);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 44);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // torzs
+            // 
+            this.torzs.BackColor = System.Drawing.Color.Brown;
+            this.torzs.Location = new System.Drawing.Point(89, 110);
+            this.torzs.Name = "torzs";
+            this.torzs.Size = new System.Drawing.Size(41, 299);
+            this.torzs.TabIndex = 3;
+            this.torzs.TabStop = false;
+            // 
+            // lomb
+            // 
+            this.lomb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lomb.Location = new System.Drawing.Point(25, -3);
+            this.lomb.Name = "lomb";
+            this.lomb.Size = new System.Drawing.Size(168, 116);
+            this.lomb.TabIndex = 4;
+            this.lomb.TabStop = false;
+            // 
+            // alma
+            // 
+            this.alma.BackColor = System.Drawing.Color.Red;
+            this.alma.Location = new System.Drawing.Point(170, 139);
+            this.alma.Name = "alma";
+            this.alma.Size = new System.Drawing.Size(23, 25);
+            this.alma.TabIndex = 5;
+            this.alma.TabStop = false;
+            // 
+            // fej
+            // 
+            this.fej.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.fej.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fej.Location = new System.Drawing.Point(401, 200);
+            this.fej.Name = "fej";
+            this.fej.Size = new System.Drawing.Size(49, 50);
+            this.fej.TabIndex = 6;
+            this.fej.TabStop = false;
+            // 
+            // test
+            // 
+            this.test.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.test.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.test.Location = new System.Drawing.Point(415, 249);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(23, 160);
+            this.test.TabIndex = 7;
+            this.test.TabStop = false;
+            // 
+            // kez
+            // 
+            this.kez.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.kez.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kez.Location = new System.Drawing.Point(389, 308);
+            this.kez.Name = "kez";
+            this.kez.Size = new System.Drawing.Size(39, 24);
+            this.kez.TabIndex = 8;
+            this.kez.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kez);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.fej);
+            this.Controls.Add(this.alma);
+            this.Controls.Add(this.lomb);
+            this.Controls.Add(this.torzs);
+            this.Controls.Add(this.kosar);
+            this.Controls.Add(this.Flor);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Flor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.torzs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fej)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kez)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Flor;
+        private System.Windows.Forms.PictureBox kosar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox torzs;
+        private System.Windows.Forms.PictureBox lomb;
+        private System.Windows.Forms.PictureBox alma;
+        private System.Windows.Forms.PictureBox fej;
+        private System.Windows.Forms.PictureBox test;
+        private System.Windows.Forms.PictureBox kez;
+    }
+}
+
